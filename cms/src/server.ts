9d7c58d4af70ba1e4df6e13d3921fcb7c2ce74ac
Samuel Poulin <samuel-poulin@outlook.com>
2023-05-25 21:53:48 -0400

    Move payload to different service

---
+++ cms/src/server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import payload from "payload";
+
+const app = express();
+
+// Redirect root to Admin panel
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
+
+const start = async () => {
+  const payloadSecret = process.env.PAYLOAD_SECRET;
+  if (!payloadSecret) throw new Error("Payload secret is not defined.");
+
+  const mongoUsername = process.env.MONGO_USERNAME;
+  const mongoPassword = process.env.MONGO_PASSWORD;
+  const mongoDatabase = process.env.MONGO_DATABASE;
+
+  if (!mongoUsername || !mongoPassword || !mongoDatabase)
+    throw new Error("Mongo options are not defined.");
+
+  const mongoURL = `mongodb://${mongoUsername}:${mongoPassword}@mongo/${mongoDatabase}`;
+
+  await payload.init({
+    secret: payloadSecret,
+    mongoURL,
+    mongoOptions: {
+      authSource: "admin",
+    },
+    express: app,
+  });
+
+  app.listen(3001);
+};
+
+start();
