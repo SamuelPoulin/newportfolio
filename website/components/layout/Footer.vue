ad73a12d23909cc484da3673baa41dbf19987544
Samuel Poulin <samuel-poulin@outlook.com>
2023-05-28 20:40:09 -0400

    Add footer

--- website/components/layout/Footer.vue
+++ website/components/layout/Footer.vue
@@ -1,15 +1,85 @@
 <template>
-    <div id="footer-container">
-        footer
+  <div id="footer-container">
+    <div id="footer-love-container">
+      <div>Made with</div>
+      <div id="footer-love-heart">💖</div>
+      <div>from Montréal</div>
     </div>
+    <div id="footer-top-container">
+      <button id="footer-top-button" v-on:click="scrollToTop">
+        back to top
+      </button>
+      <ArrowUp />
+    </div>
+    <div id="footer-copyright">© 2023 Samuel Poulin</div>
+  </div>
 </template>
 
 <style>
-#footer-container {
-    display: flex;
-    align-items: center;
+@keyframes pulse {
+  0% {
+    transform: scale(1.2);
+  }
 
-    height: 100px;
-    width: 100%;
+  50% {
+    transform: scale(1);
+  }
+
+  100% {
+    transform: scale(1.2);
+  }
+}
+
+#footer-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 100px;
+  width: 100%;
+}
+
+#footer-love-container {
+  display: flex;
+
+  gap: 10px;
+
+  font-family: Inter;
+  font-weight: 400;
+  font-size: 20px;
+}
+
+#footer-love-heart {
+  animation: 2s infinite ease-in-out pulse;
+}
+
+#footer-top-container {
+  display: flex;
+  align-items: center;
+
+  font-family: Inter;
+  font-weight: 400;
+  font-size: 20px;
+  border-bottom: 2px solid #000000;
+}
+
+#footer-top-button {
+  border: none;
+  background: none;
+  cursor: pointer;
+}
+
+#footer-copyright {
+    font-family: Inter;
+    font-weight: 700;
+    font-size: 20px;
 }
 </style>
+
+<script setup lang="ts">
+import ArrowUp from "~/components/shared/ArrowUp.vue";
+
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
+</script>
